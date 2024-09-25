acf7001bcfcc054710aae6338b188d7d038891a5
u0 <jabate@tutanota.com>
2024-09-25 02:17:10 -0400

    MLB fixes/JSON reformatting;

--- scrapers/sportsbookreview.py
+++ scrapers/sportsbookreview.py
@@ -1,9 +1,10 @@
+from datetime import datetime
 import requests
 import pandas as pd
 from itertools import tee
 import json
 import io
-
+import os
 
 class OddsScraper:
     def __init__(self, sport, years):
@@ -40,14 +41,16 @@ class OddsScraper:
     def _make_datestr(date, season, start=8, yr_end=12):
         date = str(date)
         if len(date) == 3:
-            date = f"0{date}"
+          date = f"0{date}"
         month = date[:2]
         day = date[2:]
 
         if int(month) in range(start, yr_end + 1):
-            return int(f"{season}{month}{day}")
-
-        return int(f"{int(season) + 1}{month}{day}")
+          datecode = f"{season}{month}{day}"
+        else:
+          datecode = f"{int(season+1)}{month}{day}"
+        dt_object = datetime.strptime(datecode, '%Y%m%d').date()
+        return dt_object.strftime('%Y-%m-%d')
 
     @staticmethod
     def _pairwise(iterable):
@@ -92,17 +95,17 @@ class NFLOddsScraper(OddsScraper):
             "away_4thQtr": [],
             "home_final": [],
             "away_final": [],
-            "home_close_ml": [],
-            "away_close_ml": [],
+            "ML_home_close": [],
+            "ML_away_close": [],
             "home_open_spread": [],
             "away_open_spread": [],
-            "home_close_spread": [],
-            "away_close_spread": [],
+            "SH_close_line": [],
+            "SA_close_line": [],
             "home_2H_spread": [],
             "away_2H_spread": [],
             "2H_total": [],
-            "open_over_under": [],
-            "close_over_under": [],
+            "OU_open_line": [],
+            "OU_close_line": [],
         }
 
     def _reformat_data(self, df, season):
@@ -159,8 +162,8 @@ class NFLOddsScraper(OddsScraper):
 
             home_open_spread = -open_spread if home_ml < away_ml else open_spread
             away_open_spread = -home_open_spread
-            home_close_spread = -close_spread if home_ml < away_ml else close_spread
-            away_close_spread = -home_close_spread
+            SH_close_line = -close_spread if home_ml < away_ml else close_spread
+            SA_close_line = -SH_close_line
             h2_home_spread = -h2_spread if home_ml < away_ml else h2_spread
             h2_away_spread = -h2_home_spread
 
@@ -178,17 +181,17 @@ class NFLOddsScraper(OddsScraper):
             new_df["away_4thQtr"].append(row["4thQtr"])
             new_df["home_final"].append(next_row["final"])
             new_df["away_final"].append(row["final"])
-            new_df["home_close_ml"].append(home_ml)
-            new_df["away_close_ml"].append(away_ml)
+            new_df["ML_home_close"].append(home_ml)
+            new_df["ML_away_close"].append(away_ml)
             new_df["home_open_spread"].append(home_open_spread)
             new_df["away_open_spread"].append(away_open_spread)
-            new_df["home_close_spread"].append(home_close_spread)
-            new_df["away_close_spread"].append(away_close_spread)
+            new_df["SH_close_line"].append(SH_close_line)
+            new_df["SA_close_line"].append(SA_close_line)
             new_df["home_2H_spread"].append(h2_home_spread)
             new_df["away_2H_spread"].append(h2_away_spread)
             new_df["2H_total"].append(h2_total)
-            new_df["open_over_under"].append(open_ou)
-            new_df["close_over_under"].append(close_ou)
+            new_df["OU_open_line"].append(open_ou)
+            new_df["OU_close_line"].append(close_ou)
 
         return pd.DataFrame(new_df)
 
@@ -216,17 +219,17 @@ class NBAOddsScraper(NFLOddsScraper):
             "away_4thQtr": [],
             "home_final": [],
             "away_final": [],
-            "home_close_ml": [],
-            "away_close_ml": [],
+            "ML_home_close": [],
+            "ML_away_close": [],
             "home_open_spread": [],
             "away_open_spread": [],
-            "home_close_spread": [],
-            "away_close_spread": [],
+            "SH_close_line": [],
+            "SA_close_line": [],
             "home_2H_spread": [],
             "away_2H_spread": [],
             "2H_total": [],
-            "open_over_under": [],
-            "close_over_under": [],
+            "OU_open_line": [],
+            "OU_close_line": [],
         }
 
 
@@ -250,18 +253,18 @@ class NHLOddsScraper(OddsScraper):
             "away_3rdPeriod": [],
             "home_final": [],
             "away_final": [],
-            "home_open_ml": [],
-            "away_open_ml": [],
-            "home_close_ml": [],
-            "away_close_ml": [],
-            "home_close_spread": [],
-            "away_close_spread": [],
-            "home_close_spread_odds": [],
-            "away_close_spread_odds": [],
-            "open_over_under": [],
-            "open_over_under_odds": [],
-            "close_over_under": [],
-            "close_over_under_odds": [],
+            "ML_home_open": [],
+            "ML_away_open": [],
+            "ML_home_close": [],
+            "ML_away_close": [],
+            "SH_close_line": [],
+            "SA_close_line": [],
+            "SH_close_odds": [],
+            "SA_close_odds": [],
+            "OU_open_line": [],
+            "OU_open_odds": [],
+            "OU_close_line": [],
+            "OU_close_odds": [],
         }
 
     def _reformat_data(self, df, season, covid=False):
@@ -295,20 +298,20 @@ class NHLOddsScraper(OddsScraper):
         new_df["close_spread_odds"] = new_df["close_spread_odds"].apply(
             lambda x: 0 if x in self.blacklist else float(x)
         )
-        new_df["open_over_under"] = df[12] if season > 2013 else df[10]
-        new_df["open_over_under"] = new_df["open_over_under"].apply(
+        new_df["OU_open_line"] = df[12] if season > 2013 else df[10]
+        new_df["OU_open_line"] = new_df["OU_open_line"].apply(
             lambda x: 0 if x in self.blacklist else float(x)
         )
-        new_df["open_over_under_odds"] = df[13] if season > 2013 else df[11]
-        new_df["open_over_under_odds"] = new_df["open_over_under_odds"].apply(
+        new_df["OU_open_odds"] = df[13] if season > 2013 else df[11]
+        new_df["OU_open_odds"] = new_df["OU_open_odds"].apply(
             lambda x: 0 if x in self.blacklist else float(x)
         )
-        new_df["close_over_under"] = df[14] if season > 2013 else df[12]
-        new_df["close_over_under"] = new_df["close_over_under"].apply(
+        new_df["OU_close_line"] = df[14] if season > 2013 else df[12]
+        new_df["OU_close_line"] = new_df["OU_close_line"].apply(
             lambda x: 0 if x in self.blacklist else float(x)
         )
-        new_df["close_over_under_odds"] = df[15] if season > 2013 else df[13]
-        new_df["close_over_under_odds"] = new_df["close_over_under_odds"].apply(
+        new_df["OU_close_odds"] = df[15] if season > 2013 else df[13]
+        new_df["OU_close_odds"] = new_df["OU_close_odds"].apply(
             lambda x: 0 if x in self.blacklist else float(x)
         )
 
@@ -323,7 +326,7 @@ class NHLOddsScraper(OddsScraper):
         for (i1, row), (i2, next_row) in self._pairwise(progress):
             # skip every other row
             if i1 % 2 == 0:
-                continue
+              continue
 
             new_df["season"].append(row["season"])
             new_df["date"].append(row["date"])
@@ -337,18 +340,18 @@ class NHLOddsScraper(OddsScraper):
             new_df["away_3rdPeriod"].append(row["3rdPeriod"])
             new_df["home_final"].append(next_row["final"])
             new_df["away_final"].append(row["final"])
-            new_df["home_open_ml"].append(int(next_row["open_ml"]))
-            new_df["away_open_ml"].append(int(row["open_ml"]))
-            new_df["home_close_ml"].append(int(next_row["close_ml"]))
-            new_df["away_close_ml"].append(int(row["close_ml"]))
-            new_df["home_close_spread"].append(next_row["close_spread"])
-            new_df["away_close_spread"].append(row["close_spread"])
-            new_df["home_close_spread_odds"].append(next_row["close_spread_odds"])
-            new_df["away_close_spread_odds"].append(row["close_spread_odds"])
-            new_df["open_over_under"].append(next_row["open_over_under"])
-            new_df["open_over_under_odds"].append(next_row["open_over_under_odds"])
-            new_df["close_over_under"].append(next_row["close_over_under"])
-            new_df["close_over_under_odds"].append(next_row["close_over_under_odds"])
+            new_df["ML_home_open"].append(int(next_row["open_ml"]))
+            new_df["ML_away_open"].append(int(row["open_ml"]))
+            new_df["ML_home_close"].append(int(next_row["close_ml"]))
+            new_df["ML_away_close"].append(int(row["close_ml"]))
+            new_df["SH_close_line"].append(next_row["close_spread"])
+            new_df["SA_close_line"].append(row["close_spread"])
+            new_df["SH_close_odds"].append(next_row["close_spread_odds"])
+            new_df["SA_close_odds"].append(row["close_spread_odds"])
+            new_df["OU_open_line"].append(next_row["OU_open_line"])
+            new_df["OU_open_odds"].append(next_row["OU_open_odds"])
+            new_df["OU_close_line"].append(next_row["OU_close_line"])
+            new_df["OU_close_odds"].append(next_row["OU_close_odds"])
 
         return pd.DataFrame(new_df)
 
@@ -380,51 +383,66 @@ class MLBOddsScraper(OddsScraper):
         self.base = "https://www.sportsbookreviewsonline.com/wp-content/uploads/sportsbookreviewsonline_com_737/mlb-odds-"
         self.ext = ".xlsx"
         self.schema = {
-            "season": [],
-            "date": [],
-            "home_team": [],
-            "away_team": [],
-            "home_1stInn": [],
-            "away_1stInn": [],
-            "home_2ndInn": [],
-            "away_2ndInn": [],
-            "home_3rdInn": [],
-            "away_3rdInn": [],
-            "home_4thInn": [],
-            "away_4thInn": [],
-            "home_5thInn": [],
-            "away_5thInn": [],
-            "home_6thInn": [],
-            "away_6thInn": [],
-            "home_7thInn": [],
-            "away_7thInn": [],
-            "home_8thInn": [],
-            "away_8thInn": [],
-            "home_9thInn": [],
-            "away_9thInn": [],
-            "home_final": [],
-            "away_final": [],
-            "home_open_ml": [],
-            "away_open_ml": [],
-            "home_close_ml": [],
-            "away_close_ml": [],
-            "home_close_spread": [],
-            "away_close_spread": [],
-            "home_close_spread_odds": [],
-            "away_close_spread_odds": [],
-            "open_over_under": [],
-            "open_over_under_odds": [],
-            "close_over_under": [],
-            "close_over_under_odds": [],
+          "season": [],
+          "date": [],
+          "away": [],
+          "home": [],
+          "away_final": [],
+          "home_final": [],
+          "away_pName": [],
+          "away_pThrow": [],
+          "home_pName": [],
+          "home_pThrow": [],
+          "aI1": [],
+          "aI2": [],
+          "aI3": [],
+          "aI4": [],
+          "aI5": [],
+          "aI6": [],
+          "aI7": [],
+          "aI8": [],
+          "aI9": [],
+          "hI1": [],
+          "hI2": [],
+          "hI3": [],
+          "hI4": [],
+          "hI5": [],
+          "hI6": [],
+          "hI7": [],
+          "hI8": [],
+          "hI9": [],
+          "ML_away_open": [],
+          "ML_home_open": [],
+          "ML_away_close": [],
+          "ML_home_close": [],
+          "SA_close_line": [],
+          "SA_close_odds": [],
+          "SH_close_line": [],
+          "SH_close_odds": [],
+          "OU_open_line": [],
+          "OU_open_odds": [],
+          "OU_close_line": [],
+          "OU_close_odds": [],
         }
 
     def _reformat_data(self, df, season):
         new_df = pd.DataFrame()
-        new_df["season"] = [season] * len(df)
-        new_df["date"] = df[0].apply(
-            lambda x: self._make_datestr(x, season, start=3, yr_end=10)
-        )
+        result = lambda x,y : f"{x} is smaller than {y}" if x < y \
+          else (f"{x} is greater than {y}" if x > y \
+          else f"{x} is equal to {y}")
+        print(df)
+        new_df["season"] = df[0].apply(lambda x: season)
+        new_df["date"] = df[0].apply(lambda x: self._make_datestr(x, season, start=3, yr_end=11))
         new_df["name"] = df[3]
+        new_df["pName"] = df[4].apply(lambda x: \
+          None if (type(x) != str) else \
+          x[:-2] if ("-L" in x or "-R" in x) else \
+          x
+        )
+        new_df["pThrow"] = df[4].apply(lambda x: \
+          None if (type(x) != str) else \
+          'L' if ("-L" in x) else 'R'
+        )
         new_df["1stInn"] = df[5]
         new_df["2ndInn"] = df[6]
         new_df["3rdInn"] = df[7]
@@ -439,72 +457,85 @@ class MLBOddsScraper(OddsScraper):
         new_df["close_ml"] = df[16]
         new_df["close_spread"] = df[17] if season > 2013 else 0
         new_df["close_spread_odds"] = df[18] if season > 2013 else 0
-        new_df["open_over_under"] = df[19] if season > 2013 else df[17]
-        new_df["open_over_under_odds"] = df[20] if season > 2013 else df[18]
-        new_df["close_over_under"] = df[21] if season > 2013 else df[19]
-        new_df["close_over_under_odds"] = df[22] if season > 2013 else df[20]
-
+        new_df["OU_open_line"] = df[19] if season > 2013 else df[17]
+        new_df["OU_open_odds"] = df[20] if season > 2013 else df[18]
+        new_df["OU_close_line"] = df[21] if season > 2013 else df[19]
+        new_df["OU_close_odds"] = df[22] if season > 2013 else df[20]
+        print(new_df)
         return new_df
 
     def _to_schema(self, df):
         new_df = self.schema.copy()
-        progress = df.iterrows()
-        # remove the first row, as it is the header
-        next(progress)
-        for (i1, row), (i2, next_row) in self._pairwise(progress):
-            if i1 % 2 != 0:
-                continue
+        df = df.reset_index(drop=True)
+        for i in range(len(df)):
+          if i % 2 == 0:
+            row = df.loc[i+0]
+            next_row = df.loc[i+1]
 
             new_df["season"].append(row["season"])
             new_df["date"].append(row["date"])
-            new_df["home_team"].append(self._translate(next_row["name"]))
-            new_df["away_team"].append(self._translate(row["name"]))
-            new_df["home_1stInn"].append(next_row["1stInn"])
-            new_df["away_1stInn"].append(row["1stInn"])
-            new_df["home_2ndInn"].append(next_row["2ndInn"])
-            new_df["away_2ndInn"].append(row["2ndInn"])
-            new_df["home_3rdInn"].append(next_row["3rdInn"])
-            new_df["away_3rdInn"].append(row["3rdInn"])
-            new_df["home_4thInn"].append(next_row["4thInn"])
-            new_df["away_4thInn"].append(row["4thInn"])
-            new_df["home_5thInn"].append(next_row["5thInn"])
-            new_df["away_5thInn"].append(row["5thInn"])
-            new_df["home_6thInn"].append(next_row["6thInn"])
-            new_df["away_6thInn"].append(row["6thInn"])
-            new_df["home_7thInn"].append(next_row["7thInn"])
-            new_df["away_7thInn"].append(row["7thInn"])
-            new_df["home_8thInn"].append(next_row["8thInn"])
-            new_df["away_8thInn"].append(row["8thInn"])
-            new_df["home_9thInn"].append(next_row["9thInn"])
-            new_df["away_9thInn"].append(row["9thInn"])
-            new_df["home_final"].append(next_row["final"])
+            new_df["away"].append(self._translate(row["name"]))
+            new_df["home"].append(self._translate(next_row["name"]))
             new_df["away_final"].append(row["final"])
-            new_df["home_open_ml"].append(next_row["open_ml"])
-            new_df["away_open_ml"].append(row["open_ml"])
-            new_df["home_close_ml"].append(next_row["close_ml"])
-            new_df["away_close_ml"].append(row["close_ml"])
-            new_df["home_close_spread"].append(next_row["close_spread"])
-            new_df["away_close_spread"].append(row["close_spread"])
-            new_df["home_close_spread_odds"].append(next_row["close_spread_odds"])
-            new_df["away_close_spread_odds"].append(row["close_spread_odds"])
-            new_df["open_over_under"].append(next_row["open_over_under"])
-            new_df["open_over_under_odds"].append(next_row["open_over_under_odds"])
-            new_df["close_over_under"].append(next_row["close_over_under"])
-            new_df["close_over_under_odds"].append(next_row["close_over_under_odds"])
+            new_df["home_final"].append(next_row["final"])
+            new_df["away_pName"].append(row["pName"])
+            new_df["away_pThrow"].append(row["pThrow"])
+            new_df["home_pName"].append(next_row["pName"])
+            new_df["home_pThrow"].append(next_row["pThrow"])
+            new_df["aI1"].append(row["1stInn"])
+            new_df["aI2"].append(row["2ndInn"])
+            new_df["aI3"].append(row["3rdInn"])
+            new_df["aI4"].append(row["4thInn"])
+            new_df["aI5"].append(row["5thInn"])
+            new_df["aI6"].append(row["6thInn"])
+            new_df["aI7"].append(row["7thInn"])
+            new_df["aI8"].append(row["8thInn"])
+            new_df["aI9"].append(row["9thInn"])
+            new_df["hI1"].append(next_row["1stInn"])
+            new_df["hI2"].append(next_row["2ndInn"])
+            new_df["hI3"].append(next_row["3rdInn"])
+            new_df["hI4"].append(next_row["4thInn"])
+            new_df["hI5"].append(next_row["5thInn"])
+            new_df["hI6"].append(next_row["6thInn"])
+            new_df["hI7"].append(next_row["7thInn"])
+            new_df["hI8"].append(next_row["8thInn"])
+            new_df["hI9"].append(next_row["9thInn"])
+            new_df["ML_away_open"].append(row["open_ml"])
+            new_df["ML_home_open"].append(next_row["open_ml"])
+            new_df["ML_away_close"].append(row["close_ml"])
+            new_df["ML_home_close"].append(next_row["close_ml"])
+            new_df["SA_close_line"].append(row["close_spread"])
+            new_df["SA_close_odds"].append(row["close_spread_odds"])
+            new_df["SH_close_line"].append(next_row["close_spread"])
+            new_df["SH_close_odds"].append(next_row["close_spread_odds"])
+            new_df["OU_open_line"].append(next_row["OU_open_line"])
+            new_df["OU_open_odds"].append(next_row["OU_open_odds"])
+            new_df["OU_close_line"].append(next_row["OU_close_line"])
+            new_df["OU_close_odds"].append(next_row["OU_close_odds"])
+
+        print(pd.DataFrame(new_df))
 
         return pd.DataFrame(new_df)
 
     def driver(self):
         dfs = pd.DataFrame()
+        if not os.path.isdir('data/src'):
+           os.mkdir('data/src')
         for season in self.seasons:
+          src_file = 'data/src/mlb-odds-%d.xlsx' % season
+          if os.path.isfile(src_file):
+            print('found %s, skipping download' % src_file)
+          else:
+            print('downloading %s' % src_file)
             url = self.base + str(season) + self.ext
             headers = {"User-Agent": "Mozilla/5.0"}
             r = requests.get(url, headers=headers)
+            with open(src_file, "wb") as f:
+              f.write(r.content)
 
-            with io.BytesIO(r.content) as fh:
-                df = pd.read_excel(fh, header=None, sheet_name=None)
-            dfs = pd.concat(
-                [dfs, self._reformat_data(df["Sheet1"][1:], season)], axis=0
-            )
+          df = pd.read_excel(src_file, header=None, sheet_name=None)
+          dfs = pd.concat(
+            [dfs, self._reformat_data(df["Sheet1"][1:], season)], axis=0
+          )
 
         return self._to_schema(dfs)
